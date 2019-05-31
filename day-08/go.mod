56ae3a6aaabbf81987b192ee7acf69a2db66107d
Hasitha Liyanage <hasithaliyan@gmail.com>
2019-05-31 13:30:47 +0530

    day 8

---
+++ day-08/go.mod
@@ -0,0 +1,5 @@
+module day-08
+
+go 1.12
+
+require github.com/gin-gonic/gin v1.4.0
---
+++ day-08/main.go
@@ -0,0 +1,42 @@
+/*
+DAY 8: Using a third party package!
+
+$ mkdir day-08
+$ cd day-08
+$ go mod init day-08
+$ vi main.go # copy the code below
+$ go get
+$ go build
+$ ./day-08
+$ curl http://localhost:8080/hello/world
+*/
+
+package main
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+  r := gin.Default()
+
+  r.GET("/hello/:name", func(c *gin.Context) {
+    c.JSON(http.StatusOK, gin.H{
+      "message":  "Hello, " + c.Param("name"),
+    })
+  })
+
+  r.Run()
+}
+
+/*
+EXPLANATION:
+- Go's equivalent of Express.js: https://github.com/gin-gonic/gin
+- Go's equivalent of npm init: go mod init <package-name>
+- Go's equivalent of package.json: go.mod (created by go mod init)
+- Go's equivalent of npm install: go get (it reads your import statements directly)
+- Go's equivalent of package-lock.json: go.sum (created by go get)
+- When you run the sample, don't forget to checkout the server console output!!
+- The Gin API is pretty intuitive - check out the docs.
+*/
