a83d77b23fb2cb6873406eadb4037133b5da62aa
Hasitha Liyanage <hasithaliyan@gmail.com>
2019-05-31 13:28:00 +0530

    day 1

---
+++ day-01/hello.go
@@ -0,0 +1,28 @@
+ /*
+	Day 1: Hello World
+	
+  https://golang.org/dl/ : download and install golang for your OS
+
+	$ cd your/src/directory
+	$ mkdir learning-golang
+	$ cd learning-golang
+	$ vi hello.go            # copy and paste this code
+	$ go run hello.go        # run directly (doesn't create the compiled exec)
+ */
+ 
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("hello, world\n")
+}
+
+/*
+NOTES:
+
+- Packages, import and main are kind of like in Java. We'll revisit later
+- "fmt" is a standard library package
+- main() should always be in a package named "main"
+
+*/
