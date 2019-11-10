078fd2655208f8e0cd5f406ec63fd68e43c87839
Hasitha Liyanage <hasithaliyan@gmail.com>
2019-11-10 21:53:59 +0530

    Added samples/errors

---
+++ samples/errors.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type myError struct {
+	Code    int
+	Message string
+}
+
+func (e *myError) Error() string {
+	return fmt.Sprintf("%d: %s", e.Code, e.Message)
+}
+
+func test() (int, error) {
+	return 0, &myError{Code: 1, Message: "Not implemented"}
+}
+
+func main() {
+	_, err := test()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
